1dce84d4626020fd4df768a6fcffe2a34aa3d74a
ryanadams <rcoleman.adams@gmail.com>
2022-04-14 16:20:56 -0500

    commit tree bbe5fa778d2b7acb6d7ebdfc529a21fb71ce06e2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener('DOMContentLoaded', 
+fetch(imgUrl)
+.then(resp=>resp.json()
+.then(data => addImg(data))));
+
+const addImg = data =>{
+  data.message.forEach(url=>{
+    const div = document.getElementById('dog-image-container')
+    const img = document.createElement('img')
+    img.src = url
+    div.append(img)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', 
+fetch(breedUrl)
+.then(resp=>resp.json()
+.then(data => addBreeds(data))));
+
+const addBreeds =(data)=>{
+  for(arr in data.message){
+    console.log(data.message[arr])
+  }
+
+    // li.addEventListener('click',()=>{
+    //   li.style.color = "purple"
+    // })
+
+    // ul.append(li)
+  
+}
+
+// Object.keys(data.message).forEach(dog =>{
+//   const li = document.createElement('li')
+//   const ul = document.getElementById('dog-breeds')
+//   li.textContent = dog
